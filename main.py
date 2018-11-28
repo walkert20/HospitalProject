1de06f7a29710558733420b6fc9f6da8efbc633e
Tyler Walker <33295864+walkert20@users.noreply.github.com>
2018-11-28 10:39:46 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, make_response, json, url_for, abort
+from db import Db   # See db.py
+import utils
+import json
+
+app = Flask(__name__)
+db = Db()
+app.debug = True # Comment out when not testing
+app.url_map.strict_slashes = False   # Allows for a trailing slash on routes
+
+#### ERROR HANDLERS
+
+@app.errorhandler(500)
+def server_error(e):
+   return make_json_response({ 'error': 'unexpected server error' }, 500)
+
+@app.errorhandler(404)
+def not_found(e):
+   return make_json_response({ 'error': e.description }, 404)
+
+@app.errorhandler(403)
+def forbidden(e):
+   return make_json_response({ 'error': e.description }, 403)
+
+@app.errorhandler(400)
+def client_error(e):
+   return make_json_response({ 'error': e.description }, 400)
+   
+## Helper method for creating JSON responses
+def make_json_response(content, response = 200, headers = {}):
+   headers['Content-Type'] = 'application/json'
+   return make_response(json.dumps(content), response, headers)
+   
+#### MAIN ROUTES
+
+
+
+# Starts the application
+if __name__ == "__main__":
+   app.run()
