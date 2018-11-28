96587de0f1a29a145998b0ef73632394733cce49
Tyler Walker <33295864+walkert20@users.noreply.github.com>
2018-11-28 10:32:54 -0500

    Create db.py

---
+++ db.py
@@ -0,0 +1,17 @@
+# Sets up database
+from sqlalchemy import *
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship, sessionmaker
+from json import dumps
+
+Base = declarative_base()
+
+#Database and our interactions with it
+
+
+
+#Methods
+
+
+
+#Helper methods if needed
