cc65b0eb2d02af89327ceefa6dd8b25d77f3da9f
Tyler Walker <walkert20@hanover.edu>
2018-11-29 16:06:23 -0500

    Set up for database file.

--- db.py
+++ db.py
@@ -6,12 +6,37 @@ from json import dumps
 
 Base = declarative_base()
 
-#Database and our interactions with it
+# Doctor class
 
+
+# Patient class
+
+
+#Database and our interactions with it
+class Db:
+   def __init__(self):
+      engineName = 'sqlite:///test.db'   # Uses in-memory database
+      self.engine = create_engine(engineName)
+      self.metadata = Base.metadata
+      self.metadata.bind = self.engine
+      self.metadata.drop_all(bind=self.engine)
+      self.metadata.create_all(bind=self.engine)
+      Session = sessionmaker(bind=self.engine)
+      self.session = Session()
+
+   def commit(self):
+      self.session.commit()
+
+   def rollback(self):
+      self.session.rollback()
 
 
 #Methods
 
+# Doctor methods
+
+
+# Patient methods
 
 
 #Helper methods if needed
