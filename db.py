4171dde1c41acd2b7ad0f2749a268d4c2e24d40b
Brian Hayes <hayesb20@hanover.edu>
2018-11-29 17:46:49 -0500

    Created stuff
    Creaded a start for the doctor and paitent classes as well as creaded some method stubs for both and worked on a few of them.

--- db.py
+++ db.py
@@ -7,10 +7,29 @@ from json import dumps
 Base = declarative_base()
 
 # Doctor class
+class Doctor(Base):
+   __tablename__ ="doctor"
+
+   FirstName   = Collumn(String, nullable = False)                      #THE FIRST NAME OF THE DOCTOR
+   LastName    = Collumn(String, nullable = False)                      #THE LAST NAME OF THE DOCTOR
+   profession  = Collumn(String, nullable = False)                      #THE PROFESSION OF THE DOCTOR
+   id          = Collumn(String, nullable = False, primary_key = True)  #THE ID OF THE DOCTOR CREATED BY THE SYSTEM
+   patients    = relationship("Patient", back_populates = "doctor")     #THE RELATIONSHIP BETWEEN PAITENT AND DOCTOR
+   
 
 
 # Patient class
+class Paitent(Base):
+   __tablename__ = "patient"
 
+   FirstName         = Collumn(String, default = "Jane")                      #FIRST NAME OF PAITENT
+   LastName          = Collumn(String, default = "Doe")                       #LAST NAME OF PAITENT
+   id                = Collumn(String, nullable = False, primary_key = True)  #ID OF PAITENT
+   infirmnity        = Collumn(List, default = "unknown")                     #WHATS WRONG WITH THE PAINTENT
+   medication        = Collumn(List, default = "unknown")                     #WHAT MEDS THE PAITENT NEEDS
+   date of emition   = Collumn(DateTime.now(), nullable = False)              #DATE THE WERE EMITED
+   doctor            = relationship("Doctor", back_populates = "patients")    #WHAT DOCTOR/S THE PAITENT HAS
+   
 
 #Database and our interactions with it
 class Db:
@@ -35,8 +54,56 @@ class Db:
 
 # Doctor methods
 
+#GETS THE DOCTOR WITH THE GIVEN ID IF ONE EXISTS
+def getDoctor(self, id):
+   return self.session.query(Doctor)\
+                 .filter_by(id = id)\
+                 .one_or_none()
+
+#GIVES BACK ALL DOCTORS
+def getDoctors(self):
+   return self.session.query(Doctor).all()
+
+#DELETES A GIVEN DOCTOR
+def deleteDoctor(self, doctor):
+      return self.session.delete(doctor)
+
+def addDoctor(id, first, last, profession)
+   return self.session.add(Doctor(id = id, first = first, last = last, profession = profession))
+
 
 # Patient methods
+
+#GETS THE PAITENT WITH THE GIVEN ID IF ONE EXISTS
+def getPaitent(self, id):
+   return self.session.query(Patient)\
+                 .filter_by(id = id)\
+                 .one_or_none()
+
+#GIVES BACK ALL PAITENTS
+def getPaitents(self):
+   return self.session.query(Patient).all()
+
+#DELETES A GIVEN PAITENT
+def deletePaitent(self, patient):
+      return self.session.delete(patient)
+
+#RETURNS A PAITENTS FIRST NAME
+def paitentFirst(self)
+   return self.session.query(Patient).first()
+
+#RETURNS A PAITENTS LAST NAME
+def PaitentLast(self)
+   pass
+
+#RETURNS A PAITENTS FULL NAME
+def PaitnetFull(self)
+   first = paitentFirst
+   last = PaitentLast
+   return first + last
+
+def addPaitent(id)
+   return self.session.add(Patient(id = id))
 
 
 #Helper methods if needed
